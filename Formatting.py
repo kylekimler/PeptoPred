9d691164d4a89cde5727c69fa52796726fb60cf3
elsasserlab <elsasserlab@MarianneSiMac.dyn.scilifelab.se>
2018-03-05 14:46:00 +0100

    FormatAndFit function works, first successful input to svm

--- Formatting.py
+++ Formatting.py
@@ -6,24 +6,24 @@ import sklearn
 import sklearn.externals
 
 def parse_fasta(filename):
-    inputfasta=open(filename,'r')
-    stringmaker = [str(line[:len(line)-1]) for line in inputfasta]
-    for z in range(0,len(stringmaker)-1,3):
-        stringmaker[z]=stringmaker[z].lstrip('>')
-    parsedseqs = {stringmaker[x]: stringmaker[x+1] for x in range(0,len(stringmaker)-1,3)}
-    inputfasta.close()
-    #print(parsedseqs)
-    return parsedseqs
+	inputfasta=open(filename,'r')
+	stringmaker = [str(line.rstrip('\n')) for line in inputfasta]
+	for z in range(0,len(stringmaker)-1,3):
+		stringmaker[z]=stringmaker[z].lstrip('>')
+	parsedseqs = {stringmaker[x]: stringmaker[x+1] for x in range(0,len(stringmaker)-1,3)}
+	inputfasta.close()
+	# print(parsedseqs)
+	return parsedseqs
 
 def parse_classes(filename):
-    inputfasta=open(filename,'r')
-    stringmaker = [str(line[:len(line)-1]) for line in inputfasta]
-    for z in range(0,len(stringmaker)-1,3):
-        stringmaker[z]=stringmaker[z].lstrip('>')
-    parsedclasses = {stringmaker[x]: stringmaker[x+2] for x in range(0,len(stringmaker)-1,3)}
-    inputfasta.close()
-    #print(parsedclasses)
-    return parsedclasses
+	inputfasta=open(filename,'r')
+	stringmaker = [str(line.rstrip('\n')) for line in inputfasta]
+	for z in range(0,len(stringmaker),3):
+		stringmaker[z]=stringmaker[z].lstrip('>')
+	parsedclasses = {stringmaker[x]: stringmaker[x+2] for x in range(0,len(stringmaker),3)}
+	inputfasta.close()
+	# print(parsedclasses)
+	return parsedclasses
 
 parsedseqdict = parse_fasta('testpeps.txt')
 parsedclassdict = parse_classes('testpeps.txt')
@@ -57,8 +57,8 @@ aaIndex = {'A':(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
 SpIndex = {'S':0,'G':1}
 
 '''aaIndex2 = {'A':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7,
-            'I':8, 'K':9, 'L':10, 'M':11, 'N':12, 'P':13, 'Q':14,
-            'R':15, 'S':16, 'T':17, 'V':18, 'W':19, 'Y':20, '?':0}'''
+			'I':8, 'K':9, 'L':10, 'M':11, 'N':12, 'P':13, 'Q':14,
+			'R':15, 'S':16, 'T':17, 'V':18, 'W':19, 'Y':20, '?':0}'''
 
 
 #sklearn.feature_extraction.DictVectorizer(parse_fasta('singletest1.fasta'))
@@ -66,42 +66,43 @@ SpIndex = {'S':0,'G':1}
 #print(FE)
 
 def FormatAndFit(parsedseqs, parsedclasses, windowsize):
-    protslist = list(parsedseqs.values())
-    classeslist = list(parsedclasses.values())
-    print(classeslist)
-    #indexlist = [aaIndex(z) for z in str(parse_fasta('x').values())]
-    #print(indexlist)
-    features = []
-    classifications = []
-    for prot in protslist:
-        count = 0
-        for x in range(0,len(prot)):
-            if x<windowsize//2:
-                flist=[]
-                flist.extend(aaIndex['?']*(windowsize//2-x))
-                z=0
-                while z <= x+windowsize//2:
-                    flist.extend(aaIndex[prot[z]])
-                    z+=1
-                features.append(flist)
-            elif len(prot)-windowsize//2 > x >= (windowsize//2-1):
-                middlewindow = []
-                middlewindow.extend(zz for attribute in prot[x-(windowsize//2):x+(windowsize//2)+1] for zz in aaIndex[attribute])
-                features.append(middlewindow)
-            elif(x>=(len(prot)-windowsize//2)-1):
-                elist=[]
-                z = x - (windowsize//2)
-                while z <= len(prot) - 1:
-                    elist.extend(aaIndex[prot[z]])
-                    z+=1
-                elist.extend(aaIndex['?'] * (x+windowsize//2-(len(prot)-1)))
-                features.append(elist)
-        #classifications.append(xx for attribute in parsedclasses(parsedseqs.key(prot)) for xx in SpIndex[attribute])
-        print(parsedclasses(parsedseqs[prot]))
-        #print(classifications)
-        count+=1
-    clf = svm.SVC()
-    clf.fit(features,classifications)
-    return clf
+	protslist = list(parsedseqs.values())
+	classeslist = list(parsedclasses.values())
+	#print(classeslist)
+	#indexlist = [aaIndex(z) for z in str(parse_fasta('x').values())]
+	#print(indexlist)
+	features = []
+	classifications = []
+	for key in parsedseqs:
+		count = 0
+		#print(parsedseqs[key])
+		for x in range(0,len(parsedseqs[key])):
+			if x<windowsize//2:
+				flist=[]
+				flist.extend(aaIndex['?']*(windowsize//2-x))
+				z=0
+				while z <= x+windowsize//2:
+					flist.extend(aaIndex[parsedseqs[key][z]])
+					z+=1
+				features.append(flist)
+			elif len(parsedseqs[key])-windowsize//2 > x >= (windowsize//2-1):
+				middlewindow = []
+				middlewindow.extend(zz for attribute in parsedseqs[key][x-(windowsize//2):x+(windowsize//2)+1] for zz in aaIndex[attribute])
+				features.append(middlewindow)
+			elif(x>=(len(parsedseqs[key])-windowsize//2)-1):
+				elist=[]
+				z = x - (windowsize//2)
+				while z <= len(parsedseqs[key]) - 1:
+					elist.extend(aaIndex[parsedseqs[key][z]])
+					z+=1
+				elist.extend(aaIndex['?'] * (x+windowsize//2-(len(parsedseqs[key])-1)))
+				features.append(elist)
+		classified = [SpIndex[attribute] for attribute in parsedclasses[key]]
+		#print(parsedclasses[key])
+		classifications.extend(classified)
+		count+=1
+	clf = svm.SVC()
+	clf.fit(features,classifications)
+	return clf
 
 print(FormatAndFit(parsedseqdict,parsedclassdict,5))
