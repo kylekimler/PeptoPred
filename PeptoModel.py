2c1b6130ce1e5cff210636b92b8f7bbaf778d581
elsasserlab <elsasserlab@MarianneSiMac.dyn.scilifelab.se>
2018-03-13 14:56:08 +0100

    committing latest modeller to clone to laptop

--- PeptoModel.py
+++ PeptoModel.py
@@ -24,5 +24,19 @@ model = clf.fit(Features, Classifications)
 with open('FullModelDTC%s.pkl' %int(sys.argv[1]), 'wb') as oot:
 	joblib.dump(model, oot)
 
-print("\nModeling took %0.2f seconds.\n" %(time.time()-call))
+modtime = time.time()-call
+print("\nModeling took %0.2f seconds.\n" %(modtime))
 
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import matthews_corrcoef
+
+print("Cross Validating, Do Not Distorb!1!!1")
+
+x = cross_val_predict(model, Features, Classifications, cv = 5, verbose = True)
+
+print(cross_val_score(model, Features, Classifications, cv = 5, verbose = True))
+print(x)
+print(matthews_corrcoef(Classifications, x))
+
+print("\nCross-Validation took %0.2f seconds.\n" %(time.time()-modtime))
